efb73aa107e8b7cb80754aa7730012e2ce2721f9
elver <joelros2003@gmail.com>
2024-02-08 20:37:02 +0100

    reading.txt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-use std::env;
-use std::io::Error;
+use std::{env, fs};
+use std::error::Error;
+use std::fmt;
+use std::fs::File;
+use std::io::Read;
 
-fn main() -> Result<(),Error> {
-    for arg in env::args(){
-        println!("{}", arg);
+
+#[derive(Debug, Clone)]
+struct WrongParamsErrror;
+impl Error for WrongParamsErrror{}
+
+impl fmt::Display for WrongParamsErrror {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "wrong parameters passed, it usess one extra parameter to specify the file to open")
     }
+}
+
+fn main() -> Result<(),Box<dyn Error>> {
+    if env::args().count()!=2 {
+        return Err(Box::new(WrongParamsErrror))
+    }
+    //let f: File = File::open(env::args().last().unwrap())?;
+    println!("{}",fs::read_to_string(env::args().last().unwrap())?);
+    loop{}
     Ok(())
 }
